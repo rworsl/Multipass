700ea10b5c6e9619b4ea7bd04a286678451d9668
Rich <Rich@DESKTOP-O9UJLSI>
2021-02-15 21:29:22 +0000

    mobile app

---
+++ MultiPass/MultiPass/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MultiPass.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MultiPass.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MultiPass/MultiPass/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MultiPass.Models;
+using MultiPass.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MultiPass.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
